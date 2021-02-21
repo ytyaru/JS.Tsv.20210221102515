45fc83b55a749fc877be3430dbed08b8f49f499e
ytyaru <yttry0@gmail.com>
2021-02-21 18:31:58 +0900

    number

---
+++ docs/1/js/tsvparser/TsvColumn.js
@@ -0,0 +1,31 @@
+class TsvColumn {
+    static fromTsv(lines) {
+        if (1 === lines.length)
+    }
+    static toArray(source) {
+        const lines = source.split('\n');
+        const areas = this.#splitAreas(lines);
+        console.log(areas);
+    }
+    static #splitAreas(lines) {
+        const areas = [];
+        let area = [];
+        for (const line of lines) {
+            if (0 === line.length) { areas.push(area); area = []; }
+            else { area.push(line); }
+        }
+        return areas.filter(area=>0<area.length);
+    }
+    static fromArray(source) {
+
+    }
+    static #toArrayFromString(source) {
+
+    }
+    static #toArrayFromMap(source) {
+
+    }
+    static #toArrayFromObject(source) {
+
+    }
+}
---
+++ docs/1/main.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', async(event) => {
+    for (const name of ['test-row.tsv', 'test-column1-row.tsv', 'test-column-row.tsv', 'test-column-table-row.tsv']) {
+        console.log(name);
+        TsvString.toArray(await FileLoader.load(`../tsv/${name}`));
+    }
+});
