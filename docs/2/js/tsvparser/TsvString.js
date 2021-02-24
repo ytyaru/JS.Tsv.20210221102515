f30ca1f408dbf35bc0b5631ed0d0a01c237c6434
ytyaru <ytyaru@outlook.jp>
2021-02-24 13:27:48 +0900

    ES Module + TypeFormat

---
+++ docs/2/js/tsvparser/TsvString.js
@@ -0,0 +1,31 @@
+import Column from './Column.js';
+export default class TsvString {
+    static toArray(source) {
+        const lines = source.split('\n');
+        const areas = this.#splitAreas(lines);
+        console.log(areas);
+        const column = Column.fromTsv(areas);
+        console.log(column);
+    }
+    static #splitAreas(lines) {
+        const areas = [];
+        let area = [];
+        for (const line of lines) {
+            if (0 === line.length) { areas.push(area); area = []; }
+            else { area.push(line); }
+        }
+        return areas.filter(area=>0<area.length);
+    }
+    static fromArray(source) {
+
+    }
+    static #toArrayFromString(source) {
+
+    }
+    static #toArrayFromMap(source) {
+
+    }
+    static #toArrayFromObject(source) {
+
+    }
+}
