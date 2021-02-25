a0119d0d22c5c36c66d4e4a6863d8d3dbeca4d94
ytyaru <ytyaru@outlook.jp>
2021-02-25 09:22:28 +0900

    tsv filter

---
+++ docs/5/js/tsvparser/DataTable.js
@@ -0,0 +1,7 @@
+import DateFormat from '../typeformat/DateFormat.js';
+import TableState from '../typeformat/TableState.js';
+export default class DataTable { // TableStateに沿ってTsvTableを変更して新たなテーブルデータを返す。
+    static get(source) { // source:{columns:, rows:, state:}
+        const result = {}
+    }
+}
